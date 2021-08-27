d62bdd50184b2237c91425042461107dc5ff5553
Japneet Singh <singhjapneetssa@gmail.com>
2021-08-27 11:55:26 +0530

    Created Wrapper Py scripts, compression number now fetched from inv indx,but buggy

--- invidx_cons.py
+++ invidx_cons.py
@@ -3,13 +3,191 @@ from PorterStemmer import PorterStemmer
 from collections import defaultdict
 import sys
 import re
+import snappy
 import os
 import json
 import string
 
-coll_path = sys.argv[0]
-indexfile = sys.argv[1]
-stopwordfile = sys.argv[2]
-compression = int(sys.argv[3])
-xml_tags_info = sys.argv[4]
+def def_value():
+    return "Not Present"
 
+def c1_encode(n):
+    byte_arr = [] 
+    byte_arr.append(n%128)
+    n = n//128
+    while(True):
+        if(n==0):           
+            break
+        else: 
+            byte_arr.insert(0, n%128 + 128)
+            n = n//128
+    return byte_arr
+
+def c2_encode(x):
+    encoded = ""
+    lx = x.bit_length()
+    llx = lx.bit_length()
+    for _ in range(0, llx-1):
+        encoded += '1'
+    encoded += '0'
+    a1 = lx
+    b1 = llx-1
+    temp1 = ""
+    for _ in range(0,b1):
+        temp1 = str(a1%2) + temp1
+        a1 = a1//2
+    a2 = x
+    b2 = lx-1
+    temp2 = ""
+    for _ in range(0,b2):
+        temp2 = str(a2%2) + temp2
+        a2 = a2//2
+    encoded += temp1
+    encoded += temp2
+    return encoded
+
+
+if __name__ == '__main__':
+
+    ps = PorterStemmer()
+    docId = defaultdict(def_value)
+    postings = defaultdict(list)
+    count = 0
+    lastDoc = defaultdict(int)
+    offsetAndLength = defaultdict(lambda: [0,0])
+
+    exclist = string.punctuation 
+    table = str.maketrans('', '', exclist)
+
+    coll_path = sys.argv[1]
+    index_file = sys.argv[2]
+    stopword_file = sys.argv[3]
+    c_no = int(sys.argv[4])
+    xml_tags_info = sys.argv[5]
+
+    if(c_no>=4):
+        print('not implemented')
+        exit()
+
+    xmltags = []
+    with open(xml_tags_info, 'r') as f:
+        for line in f:
+            temp = line.rstrip()
+            if(temp!='DOCNO'):
+                xmltags.append(temp.lower())
+
+    filecount = 0
+    doclist = os.listdir(coll_path)
+    total = len(doclist)
+
+    for file in doclist:
+        filecount += 1
+        f = os.path.join(coll_path, file)
+        if(file=='ap890520'): 
+            continue
+        if(filecount>20):
+            break
+        xmldoc = open(f, 'r')
+        soup = BeautifulSoup(xmldoc, 'html.parser')
+        docs = soup.find_all('doc')
+        print('Processing ', f, '( ', filecount, ' out of ', total, ' processed )')
+        for doc in docs:
+            count += 1
+            docNo = doc.find('docno')
+            # heads = doc.find_all('head')
+            # texts = doc.find_all('text')
+            id = docNo.get_text().replace(' ', '')
+            docId[count] = id
+            for xmltag in xmltags:
+                tag_list = doc.find_all(xmltag)
+                if(len(tag_list)>0):
+                    for tag_obj in tag_list:
+                        temp = tag_obj.get_text().translate(str.maketrans(table)).split()
+                        # temp = modText.split()
+                        # temp = re.split(r'[`\-=~!@#$%^&*()_+\[\]{};\'\\:"|<,./<>?\s]', head.get_text())
+                        for word in temp:
+                            if(word=='' or word==' ' or word=='  ' or word.isnumeric()):
+                                continue
+                            stemmed = ps.stem(word.lower(), 0, len(word)-1)
+                            # print(stemmed)
+                            if(len(postings[stemmed])==0):
+                                postings[stemmed].append(count)
+                                lastDoc[stemmed]=count   
+                            elif(lastDoc[stemmed]!=count):
+                                postings[stemmed].append(count-lastDoc[stemmed]) 
+                                lastDoc[stemmed]=count    
+
+    destFile = open(index_file+'.idx', "wb")
+    destFile.write(c_no.to_bytes(1, sys.byteorder))
+    cOffset = 1
+    encodedJSON = json.dumps(docId).encode('utf-8')
+    destFile.write(encodedJSON)
+    offsetAndLength['DocIdMapLength'][0] = 1
+    offsetAndLength['DocIdMapLength'][1] = len(encodedJSON)
+    cOffset += len(encodedJSON)
+
+    if(c_no==0):
+        for key in postings.keys():
+            offsetAndLength[key][0]=cOffset
+        pl = postings[key]
+        toWrite=''
+        for i in range(0,len(pl)):
+            toWrite+=str(pl[i])
+            if(i!=len(pl)-1):
+                toWrite+=','
+        toWrite = toWrite.encode('utf8')
+        cOffset+=len(toWrite)
+        offsetAndLength[key][1]=len(toWrite)
+        destFile.write(toWrite)
+    elif(c_no==1):
+        for key in postings.keys():
+            offsetAndLength[key][0]=cOffset
+        pl = postings[key]
+        for post in pl:
+            encoded = c1_encode(post)
+            for j in range(0, len(encoded)):
+                toWrite = encoded[j].to_bytes(1, sys.byteorder)
+                cOffset+=1
+                offsetAndLength[key][1]+=1
+                destFile.write(toWrite)   
+    elif(c_no==2):
+        for key in postings.keys():
+            offsetAndLength[key][0]=cOffset
+        pl = postings[key]
+        toWrite = ''
+        for post in pl:
+            toWrite+=c2_encode(post)
+        # print(toWrite)
+        padding = (8 - (len(toWrite)%8))%8
+        toWrite+=('1'*padding)
+        bytesList = [toWrite[i:i+8] for i in range(0, len(toWrite), 8)]
+        bytesList = [int(ele, 2) for ele in bytesList]
+        # print(bytesList)
+        cOffset+=len(bytesList)
+        offsetAndLength[key][1]=len(bytesList)
+        for posting in bytesList:
+            finalToWrite = posting.to_bytes(1, sys.byteorder)
+            destFile.write(finalToWrite)
+    elif(c_no==3):
+        for key in postings.keys():
+            offsetAndLength[key][0]=cOffset
+        pl = postings[key]
+        toWrite = ''
+        for post in pl:
+            toWrite+=str(post)
+            toWrite+=' '
+        toWrite = toWrite.encode()
+        toWrite=snappy.compress(toWrite)
+        # print(toWrite)
+        destFile.write(toWrite)
+        cOffset+=len(toWrite)
+        offsetAndLength[key][1]=len(toWrite)    
+    elif(c_no==4):
+        print('not implemented')
+    elif(c_no==5):
+        print('not_implemented')
+    else: 
+        print('not_implemented')   
+
+    with open(index_file+'.dict', "w") as fp:
+        json.dump(offsetAndLength,fp) 
---
+++ query.py
@@ -0,0 +1,211 @@
+from bs4 import BeautifulSoup
+from PorterStemmer import PorterStemmer
+from collections import defaultdict
+import snappy
+import sys
+import json
+import re
+import os
+
+
+if __name__ == '__main__':
+    queryfile = sys.argv[1]
+    resultfile = sys.argv[2]
+    indexfile = sys.argv[3]
+    dictfile = sys.argv[4]
+    c_no = -1
+
+    f = open(dictfile, 'r')
+    offsetAndLength = json.load(f)
+    docId = {}
+    DocIdMapOffset = offsetAndLength['DocIdMapLength'][0]
+    DocIdMapLength = offsetAndLength['DocIdMapLength'][1]
+
+    with open(indexfile, "rb") as f:
+        c_no = f.read(1)
+        c_no = int.from_bytes(c_no, sys.byteorder)
+        print('c', c_no)
+        jsonEncoded = f.read(DocIdMapLength)
+        jsonEncoded.decode('utf-8')
+        docId= json.loads(jsonEncoded)
+
+    if(c_no==-1):
+        print('not_implemented')
+        exit()
+    
+    ps = PorterStemmer()
+
+    list1 = []
+    list2 = []
+    term1 = 'simon'
+    term1 = ps.stem(term1.lower(), 0, len(term1)-1)
+    term2 = 'i'
+    term2= ps.stem(term2.lower(), 0, len(term2)-1)
+    offset1 = offsetAndLength[term1][0]
+    offset2 = offsetAndLength[term2][0]
+
+
+    if(c_no==1):
+        with open(indexfile, "rb") as f:
+            decoded = 0
+            totalDecoded = 0
+            f.seek(offset1)
+            while(totalDecoded<offsetAndLength[term1][1]):
+                byte = f.read(1)
+                totalDecoded += 1
+                if(not byte):
+                    break
+                readByte = int.from_bytes(byte, sys.byteorder)
+                if(readByte<128):
+                    decoded = decoded*128 + readByte
+                    list1.append(decoded)
+                    decoded = 0
+                else:
+                    decoded = decoded*128 + (readByte-128)
+        with open(indexfile, "rb") as f:
+            decoded = 0
+            totalDecoded = 0
+            f.seek(offset2)
+            while(totalDecoded<offsetAndLength[term2][1]):
+                byte = f.read(1)
+                totalDecoded += 1
+                if(not byte):
+                    break
+                readByte = int.from_bytes(byte, sys.byteorder)
+                if(readByte<128):
+                    decoded = decoded*128 + readByte
+                    list2.append(decoded)
+                    decoded = 0
+                else:
+                    decoded = decoded*128 + (readByte-128)
+    elif(c_no==2):
+        with open(indexfile, "rb") as f:
+                f.seek(offset1)
+                uncomp = ''
+                i = 0
+                while(i<offsetAndLength[term1][1]):
+                    nextByte = f.read(1)
+                    uncomp += '{0:08b}'.format(int.from_bytes(nextByte, sys.byteorder))
+                    i+=1
+                # print(uncomp)
+                iter = 0
+                while(iter<offsetAndLength[term1][1]*8):
+                    cLen = 0
+                    while(uncomp[iter]!='0'):
+                        cLen+=1
+                        iter+=1
+                        if(iter>=offsetAndLength[term1][1]*8):
+                            break
+                    if(iter>=offsetAndLength[term1][1]*8 and uncomp[-1]=='1'):
+                        break
+                    iter+=1
+                    cLen+=1
+                    llx=cLen
+                    lx=1
+                    for _ in range(0,llx-1):
+                        bit = int(uncomp[iter])
+                        lx = lx*2 + bit
+                        iter = iter+1
+                    x = 1
+                    for _ in range(lx-1):
+                        bit = int(uncomp[iter])
+                        x = x*2 + bit
+                        iter = iter+1
+                    list1.append(x)
+                    # print(x)
+                # print(list1)
+        with open(indexfile, "rb") as f:
+                f.seek(offset2)
+                uncomp = ''
+                i = 0
+                while(i<offsetAndLength[term2][1]):
+                    nextByte = f.read(1)
+                    uncomp += '{0:08b}'.format(int.from_bytes(nextByte, sys.byteorder))
+                    i+=1
+                # print(uncomp)
+                iter = 0
+                while(iter<offsetAndLength[term2][1]*8):
+                    cLen = 0
+                    while(uncomp[iter]!='0'):
+                        cLen+=1
+                        iter+=1
+                        if(iter>=offsetAndLength[term2][1]*8):
+                            break
+                    if(iter>=offsetAndLength[term2][1]*8 and uncomp[-1]=='1'):
+                        break
+                    iter+=1
+                    cLen+=1
+                    llx=cLen
+                    lx=1
+                    for _ in range(0,llx-1):
+                        bit = int(uncomp[iter])
+                        lx = lx*2 + bit
+                        iter = iter+1
+                    x = 1
+                    for _ in range(lx-1):
+                        bit = int(uncomp[iter])
+                        x = x*2 + bit
+                        iter = iter+1
+                    list2.append(x)
+                    # print(x)
+                # print(list2)       
+    elif(c_no==3):
+        with open(indexfile, "rb") as f:
+                f.seek(offset1)
+                comp = f.read(offsetAndLength[term1][1])
+                # print(comp)
+                uncomp = snappy.uncompress(comp)
+                strList1 = uncomp.decode('utf8')
+                strList1 = strList1.split(' ')
+                # print(strList1)
+                list1 = [int(ele) for ele in strList1[0:-1]]
+        with open(indexfile, "rb") as f:
+                f.seek(offset2)
+                comp = f.read(offsetAndLength[term2][1])
+                # print(comp)
+                uncomp = snappy.decompress(comp)
+                strList2 = uncomp.decode('utf8')
+                strList2 = strList2.split(' ')
+                # print(strList2)
+                list2 = [int(ele) for ele in strList2[0:-1]]
+    # print(list1)
+    # print(list2)
+    elif(c_no==4):
+        print('not implemented')
+        exit()
+    elif(c_no==5):
+        print('not implemented')
+        exit()
+    else: 
+        print('not_implemented')
+        exit()
+    
+    f1 = open(resultfile, 'w')
+    len1 = len(list1)
+    len2 = len(list2)
+    i = 0
+    j = 0
+    t1 = 0
+    t2 = 0
+    if(len1>0):
+        t1=list1[0]
+    if(len2>0):
+        t2=list2[0]
+    while(i<len1 and j<len2):
+        # print(t1, t2)
+        if(t1==t2):
+            print(docId[str(t1)], file=f1)
+            i+=1
+            j+=1
+            if(i<len1):
+                t1+=list1[i]
+            if(j<len2):
+                t2+=list2[j]
+        elif(t1<t2):
+            i+=1
+            if(i<len1):
+                t1+=list1[i]       
+        else: 
+            j+=1
+            if(j<len2):
+                t2+=list2[j]       
